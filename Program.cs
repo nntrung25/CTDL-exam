2e70ad4c21e7f4019a8ac57b6a940494dd75e3ee
nntrung47 <nntrung47@gmail.com>
2021-12-03 23:23:52 +0700

    add

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CTDL_exam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi World!");
         }
     }
 }
