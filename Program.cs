97dc9c2b5773f511f779fcd477170a607125b109
nntrung47 <nntrung47@gmail.com>
2021-12-05 13:55:56 +0700

    add

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace CTDL_exam
 {
     class Program
     {
+        // Code hàm chức năng ở dưới Hàm main
         static void Main(string[] args)
         {
             
@@ -12,13 +13,8 @@ namespace CTDL_exam
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             
             Console.WriteLine("CTDL exam");
-            Console.WriteLine("hello tui nè");
-            System.Console.WriteLine("Hello world");
-            System.Console.Writeline("Tin ne duoc chua");
-            System.Console.WriteLine("Tao ne");
-            System.Console.WriteLine("test dn");
-            
-            System.Console.WriteLine("kkkk");
+
+
             
         }
     }
